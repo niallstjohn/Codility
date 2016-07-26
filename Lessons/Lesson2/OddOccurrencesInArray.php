5afef82ad2fa455477e843984c7ff2697d35abc7
niallstjohn <niallstjohn@gmail.com>
2016-07-26 16:19:40 +0100

    Typo in OddOccurrencesInArray.php

--- Lessons/Lesson2/OddOccurrencesInArray.php
+++ Lessons/Lesson2/OddOccurrencesInArray.php
@@ -46,8 +46,8 @@ Elements of input arrays can be modified.
 Result Link: https://codility.com/demo/results/trainingCZKC43-XXK/
 Codility Level: Painless
 Detected time complexity: O(N)
-Correctness: 100%;
-Performance: 100%;
+Correctness: 100%
+Performance: 100%
 Task Score: 100%
 */
 
